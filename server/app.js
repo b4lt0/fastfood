c21f7be5f98b1dd58dae53cfda5db5d3653ec535
theman2211 <francescocarone22@gmail.com>
2022-06-06 16:30:23 +0200

    Prima configurazione mongoDB atlas

---
+++ server/app.js
@@ -0,0 +1,16 @@
+
+var mongoose = require('mongoose');
+var express = require('express');
+
+var app = express();
+
+require('dotenv').config();
+//db
+mongoose.connect(process.env.MONGO_URI, {
+}).then(() => console.log('DB CONNECTED')).catch(err => console.log("DB CONNECTION ERROR ", err));
+
+//port
+const port = process.env.PORT;
+//listener
+const server = app.listen(port, () =>
+    console.log(`Server is running on port : ${port}`));
