d34ca15667853f2d0e526d2affa74843c97ad8a6
theman2211 <francescocarone22@gmail.com>
2022-06-06 16:54:41 +0200

    correzione errori per il collegamento a MONGODB

--- server/app.js
+++ server/app.js
@@ -1,13 +1,16 @@
-
-var mongoose = require('mongoose');
-var express = require('express');
+const mongoose = require('mongoose');
+const express = require('express');
 
 var app = express();
 
 require('dotenv').config();
+
 //db
-mongoose.connect(process.env.MONGO_URI, {
-}).then(() => console.log('DB CONNECTED')).catch(err => console.log("DB CONNECTION ERROR ", err));
+
+mongoose
+    .connect(process.env.MONGO_URI, {})
+    .then(() => console.log('DB CONNECTED'))
+    .catch(err => console.log("DB CONNECTION ERROR ", err));
 
 //port
 const port = process.env.PORT;
